0bd3251182b1c16aa98f248cb8bbb3cca1c63fcf
王保清 <viprosite@outlook.com>
2023-07-14 15:00:13 +0800

    'init-antd'

---
+++ antd-learn/src/App.jsx
@@ -0,0 +1,11 @@
+import { Button, Space } from 'antd'
+
+
+export default function App() {
+    return (
+        <>
+            <Button type='primary'>Primary Button</Button>
+
+        </>
+    )
+}
---
+++ antd-learn/webpack.config.js
@@ -0,0 +1,45 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    mode: 'development',
+    entry: './src/main.js',
+    output: {
+        filename: '[name].bundle.js',
+        path: path.join(__dirname, 'dist'),
+    },
+    resolve: {
+        extensions: ['.js', '.jsx'],
+        alias: {
+            '@': path.join(__dirname, 'src')
+        }
+    },
+    module: {
+        rules: [
+            {
+                test: /.jsx?$/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: [
+                            ['@babel/preset-env'],
+                            ['@babel/preset-react', { runtime: 'automatic' }]
+                        ]
+                    }
+                }
+            },
+            {
+                test: /.css$/,
+                use: ['style-loader', 'css-loader']
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: path.join(__dirname, 'public/index.html'),
+        })
+    ],
+    devServer: {
+        historyApiFallback: true,
+    }
+}
